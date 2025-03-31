0369155ef25c709fa40930b88418d8002d0e4cc8
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-03-31 17:19:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 
 import React from "react";
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
-// const [count, setCount] = useState(0);
+const [count, setCount] = useState(0);
  const handleClick = () => {
     setCount(count + 1);
 
   
   return (
-        {/* Do not remove the main div */}
+
  <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Button Counter</h1>
       <p>Button clicked {count} times</p>
